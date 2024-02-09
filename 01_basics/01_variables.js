1adf12c2ca203feb34b25dd017edfba4cb65aa74
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2024-02-09 12:19:35 +0000

    explore about Variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const employeId = 15452;
+let employeEmail = "Skr@gmail.com";
+var password = "1546200";
+accountCity = "Jaipur";
+let employeeState; 
+
+// employeId = 145; //Not Allowed
+
+console.log(employeId);
+
+employeEmail = "angel@gmail.com";
+password = "257122";
+accountCity = "Bihar";
+
+console.table([employeId,employeEmail,password,accountCity,employeeState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
