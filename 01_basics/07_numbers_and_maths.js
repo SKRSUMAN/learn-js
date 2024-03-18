ab34e6fcaecdfc6f8c7ad7c2683b9abfb426ccb3
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2024-03-18 11:52:24 +0000

    Math_and_Numbers

---
+++ 01_basics/07_numbers_and_maths.js
@@ -0,0 +1,40 @@
+const score = 300
+console.log(score);
+
+const balance = new Number(700)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 125.80254
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundereds = 1000000
+
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// +++++++++++++++ Maths +++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(7.5));
+// console.log(Math.ceil(7.2));
+// console.log(Math.floor(7.9));
+// console.log(Math.min(4,0,6,3,8));
+// console.log(Math.max(4,0,6,3,8));
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min );
+
+
+
