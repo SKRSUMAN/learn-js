aa2235dfd8713617ee7753aa02d7fed6ce754f5e
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2024-03-18 11:09:09 +0000

    Strings Concept

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,39 @@
+const name = "Skr Suman "
+const repoCount = 8
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+
+const myGfName = new String("Angel-Anjali")
+
+// console.log(myGfName);
+// console.log(myGfName[2]);
+// console.log(myGfName.__proto__);
+// console.log(myGfName.length);
+// console.log(myGfName.toUpperCase());
+// console.log(myGfName.toLowerCase());
+// console.log(myGfName.charAt(2));
+// console.log(myGfName.indexOf('g'));
+
+// const newString = myGfName.substring(0,5)
+// console.log(newString);
+
+// const anotherString = myGfName.slice(-10,5)
+// console.log(anotherString);
+
+// const newStringOne = "   SkrSuman.      "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https:/skrSuman.com/suman%20kumar"
+// console.log(url);
+// console.log(url.replace("%20", '_'));
+// console.log(url.includes('Suman'));
+
+console.log(myGfName.split('-'));
+
+
+
+
