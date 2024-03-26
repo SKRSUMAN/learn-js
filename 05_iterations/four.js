0bfe5d1f2fd0b19ee700ee58a9d26bd5a5ac41a7
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2024-03-26 15:26:33 +0000

    for in loop in js

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javaScript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "java", "Cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+}
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA',"United state of America")
+map.set('Fr', 'France')
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
